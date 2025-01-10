3fc19b24c6c09bf827b8973cf5ab7399fcf11c2c
Elijah Gummer <elijahgummer5@gmail.com>
2025-01-10 18:28:13 +1000

    changed the code

--- car-wash-website-template/mail/contact.js
+++ car-wash-website-template/mail/contact.js
@@ -1,57 +1,36 @@
 $(function () {
-
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
-        },
-        submitSuccess: function ($form, event) {
-            event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
-
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
-
-            $.ajax({
-                url: "contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
-            });
+            // Error handling code
+            $('#success').html("<div class='alert alert-danger'>");
+            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+            $('#success > .alert-danger').append("<strong>Sorry, it seems that our mail server is not responding. Please try again later!</strong>");
+            $('#success > .alert-danger').append('</div>');
         },
         filter: function () {
             return $(this).is(":visible");
         },
+    });
+
+    // Custom validation for email format
+    $("#email").on("input", function () {
+        var email = $(this).val();
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!emailPattern.test(email)) {
+            $(this).next(".help-block").text("Please enter a valid email address.");
+        } else {
+            $(this).next(".help-block").text("");
+        }
+    });
+
+    // Custom validation for required fields
+    $("#contactForm input, #contactForm textarea").on("input", function () {
+        if ($(this).val() === "") {
+            $(this).next(".help-block").text("This field is required.");
+        } else {
+            $(this).next(".help-block").text("");
+        }
     });
 
     $("a[data-toggle=\"tab\"]").click(function (e) {
